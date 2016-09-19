c8c57d06bbd4ff7532d124572e1ccbc69770c48a
bjkangying <bjkangying@corp.netease.com>
2016-09-19 12:03:34 +0800

    Release 1.0.0.

---
+++ WZMarqueeView/MyView.h
@@ -0,0 +1,13 @@
+//
+//  MyView.h
+//  
+//
+//  Created by ciefky on 16/9/19.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyView : UIView
+
+@end
---
+++ WZMarqueeView/MyView.m
@@ -0,0 +1,21 @@
+//
+//  MyView.m
+//  
+//
+//  Created by ciefky on 16/9/19.
+//
+//
+
+#import "MyView.h"
+
+@implementation MyView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
